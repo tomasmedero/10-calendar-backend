65e25b10c971da339c34d4e5c0d7c8c50606ef00
Tomas <tomasmedero92@gmail.com>
2023-07-23 19:09:56 -0300

    Comment change

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ app.use(express.static('public'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
 
-//TODO: CRUD de Eventos
-
 //Escuchar Peticiones
 
 app.listen(process.env.PORT, () => {
