c257d8ecd2a15117d2498cdacace09dc8f04e6bc
Tomas <tomasmedero92@gmail.com>
2023-07-27 19:29:54 -0300

    fix

--- routes/events.js
+++ routes/events.js
@@ -37,7 +37,16 @@ router.post(
 )
 
 //Actualizar Evento
-router.put('/:id', actualizarEvento)
+router.put(
+  '/:id',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos,
+  ],
+  actualizarEvento
+)
 
 //Eliminar Evento
 router.delete('/:id', eliminarEvento)
