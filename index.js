e8008d2f4371989da14110ba75ee10eb362ffbcd
Tomas <tomasmedero92@gmail.com>
2023-07-27 21:04:42 -0300

    Path agregado

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use(express.static('public'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 //Escuchar Peticiones
 
 app.listen(process.env.PORT, () => {
